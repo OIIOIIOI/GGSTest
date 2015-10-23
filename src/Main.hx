6395a8919c08970938c8409ba8fbbd2be0cc2587
01101101 <sylvere.armange@gmail.com>
2015-10-23 08:50:22 +0200

    created project

---
+++ src/Main.hx
@@ -0,0 +1,21 @@
+package;
+
+import openfl.display.Sprite;
+import openfl.Lib;
+
+/**
+ * ...
+ * @author 01101101
+ */
+class Main extends Sprite 
+{
+
+	public function new() 
+	{
+		super();
+		
+		// Assets:
+		// openfl.Assets.getBitmapData("img/assetname.jpg");
+	}
+
+}
